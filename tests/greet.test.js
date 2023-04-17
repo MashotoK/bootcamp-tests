4f3161babe2037e826ca7e6209d59bd64e81d6e3
KgasagoGH <kgasago.mash@gmail.com>
2023-04-17 09:45:48 +0200

    modified greet tests and isFromBellville test

--- tests/greet.test.js
+++ tests/greet.test.js
@@ -1,9 +1,9 @@
-var assert = require('chai').assert;
-var greet = require('../greet'); // import the greet function
+const assert = require('chai').assert;
+const greet = require('../greet.js'); // import the greet function
 
 describe('greet', function() {
   it('should return a greeting with the given name', function() {
-    var result = greet('Janine');
+    const result = greet('Janine');
     assert.equal(result, 'Hello, Janine');
   });
 });
--- tests/isFromBellville.test.js
+++ tests/isFromBellville.test.js
@@ -1,14 +1,16 @@
-var assert = require('chai').assert;
-const isFromBellville = require('../isFromBellville');
+const assert = require('assert');
+const isFromBellville = require('../isFromBellville.js');
 
 describe('isFromBellville', function() {
-  it('should return true for a registration number from Bellville', function() {
-    var result = isFromBellville('CY 123 CY');
-    assert.isTrue(result);
+  it('should return true if regNumber ends with "CY"', function() {
+    const regNumber = 'CA 1234 CY';
+    const result = isFromBellville(regNumber);
+    assert.strictEqual(result, true);
   });
-
-  it('should return false for a registration number not from Bellville', function() {
-    var result = isFromBellville('CA 123 GP');
-    assert.isFalse(result);
+  
+  it('should return false if regNumber does not end with "CY"', function() {
+    const regNumber = 'CA 1234 CJ';
+    const result = isFromBellville(regNumber);
+    assert.strictEqual(result, false);
   });
 });
